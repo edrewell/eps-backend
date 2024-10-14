f528e03dce6fc28379503d59057cddd1a6b79b05
Elliott <e.l.drewell94@gmail.com>
2024-10-14 11:17:58 +0100

    Update Customer controller complete

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,87 +1,82 @@
-import Customer from "../models/customerModel.js"
+import Customer from "../models/customerModel.js";
 
 // Get all customers name & ID
 
-export const getCustomers = async(req, res) => {
-    try {
-        const customers = await Customer.find({}, "_id displayName")
-        res.status(200).json(customers)
-    } catch (error) {
-        res.status(400).json({msg:"Could not retrieve customers"})
-        console.error(error)
-    }
-
-}
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await Customer.find({}, "_id displayName");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(400).json({ msg: "Could not retrieve customers" });
+    console.error(error);
+  }
+};
 
 // Get customers by filter
 
-export const getCustomersByFilter = async(req, res) => {
-    try {
-        
-        const expression = new RegExp(`${req.body.customerFilterValue}`, "i")
-        console.log(expression)
-                                               
-        const customers = await Customer.find({[req.body.customerFilterQuery]:expression})
-        
-        if(!customers) {
-            res.status(200).json({msg: "No search results"})
-        } else {
-            res.status(200).json({customers})
-        }
+export const getCustomersByFilter = async (req, res) => {
+  try {
+    const expression = new RegExp(`${req.body.customerFilterValue}`, "i");
+    console.log(expression);
 
-    } catch (error) {
-        res.status(400).json({msg:"Could not retrieve customers"})
-        console.log(error)
+    const customers = await Customer.find({
+      [req.body.customerFilterQuery]: expression,
+    });
+
+    if (!customers) {
+      res.status(200).json({ msg: "No search results" });
+    } else {
+      res.status(200).json({ customers });
     }
-}
+  } catch (error) {
+    res.status(400).json({ msg: "Could not retrieve customers" });
+    console.log(error);
+  }
+};
 
 // Get customer by ID
 
-export const getCustomerByID = async(req, res) => {
-    try {
-        const customer = await Customer.findById(req.body.customerid)
-        if (!customer) {
-            res.status(404).json({msg: "Customer not found"})
-        } else {
-            res.status(200).json(customer)
-        }
-
-    } catch (error) {
-        res.status(400).json({msg:"Could not get Customer"})
-        console.error(error)
-        
+export const getCustomerByID = async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.body.customerid);
+    if (!customer) {
+      res.status(404).json({ msg: "Customer not found" });
+    } else {
+      res.status(200).json(customer);
     }
-}
+  } catch (error) {
+    res.status(400).json({ msg: "Could not get Customer" });
+    console.error(error);
+  }
+};
 
 // Create a new customer
 
-export const createCustomer = async(req, res) => {
-    try {
-        const customer = await Customer.create(req.body)
-        res.status(200).json({msg:"Customer created"})
-    } catch (error) {
-        console.error(error)        
-        res.status(400).json({msg:"Error creating customer"})
-    }
-}
+export const createCustomer = async (req, res) => {
+  try {
+    const customer = await Customer.create(req.body);
+    res.status(200).json({ msg: "Customer created" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "Error creating customer" });
+  }
+};
 
 // Update a customer
 
-export const updateCustomer = async(req, res) => {
-    try {
-        console.log(req.body)
-        const customer = await Customer.findById(req.body.customerid)
-        console.log(customer)
-        if (!customer) {
-            res.status(400).json({msg:"Customer not found"})
-        } else {
-            delete req.body.id
-            console.log(req.body)
-            const updatedCustomer = await Customer.findByIdAndUpdate(customer.id, req.body)
-            console.log(updatedCustomer)
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({msg: "Could not update customer"})
+export const updateCustomer = async (req, res) => {
+  try {
+    console.log(req.body);
+    const customer = await Customer.findById(req.body.id);
+    if (!customer) {
+      res.status(400).json({ msg: "Customer not found" });
+    } else {
+      await Customer.findByIdAndUpdate(customer.id, req.body);
+      const updatedCustomer = await Customer.findById(customer._id);
+      res.status(200).json({ updatedCustomer });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Could not update customer" });
+  }
+};
--- dbpop.js
+++ dbpop.js
@@ -1,6 +1,17 @@
-const thing = {
-    page: "book"
-}
+var l1 = [2, 3, 4]
+var l2 = [5, 6, 7]
 
-console.log({"test": "test"})
+l1.reverse()
+l2.reverse()
 
+console.log(l1, l2)
+
+var n1 = l1.join('')
+var n2 = l2.join('')
+console.log(n1, n2)
+
+var total = Number(n1) + Number(n2)
+console.log(total)
+
+var totalToArray = Array.from(String(total), Number)
+console.log(totalToArray.reverse())
