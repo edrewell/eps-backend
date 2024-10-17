aaaa57da8b4b92e9ca6cf567fccca9623872c8e7
Elliott <e.l.drewell94@gmail.com>
2024-10-17 22:01:34 +0100

    Payment model creation

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,1 +1,23 @@
-const mongoose = require('mongoose')
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const paymentSchema = new Schema({
+    invoiceID : {
+        type: Schema.ObjectId,
+    },
+    amount : {
+        type: Number,
+        required: true
+    },
+    dateReceived: {
+        type: Date,
+        required: true 
+    }
+})
+
+
+
+const Payment = mongoose.Schema("Payment", paymentSchema)
+
+export default Payments
